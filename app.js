243419116bf331a1112677c960b9fd6ffe4ce1a2
CritterCodes <jacobaengel55@gmail.com>
2024-08-23 11:57:00 -0500

    updated cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: "https://relics.engelfinedesign.com"
 }));
 
 app.use('/api/v1/algo', algoR);
